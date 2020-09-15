d5258d2bd7f5a9a834a39714792df5d3155eef90
Clément Ballet <clementballet@gmail.com>
2020-09-15 16:25:55 +0200

    Update to bewwwa mockup

--- js/backToTop.js
+++
@@ -1,7 +0,0 @@
-function init(htmlElement) {
-    document.addEventListener('DOMContentLoaded', function() {
-        window.onscroll = function(ev) {
-            htmlElement.className = (window.pageYOffset > 100) ? "back-to-top visible" : "back-to-top invisible";
-        };
-    });
-}
